7f9c36779bffc145d6edd0373ab726b6c688d6b5
Bo Tranberg <bo@tberg.dk>
2014-07-28 12:04:56 +0200

    added construction of dictionary

--- link_namer.py
+++ link_namer.py
@@ -12,8 +12,7 @@ from link_colour_less import track_flows, get_link_direction
 from new_linkcolouralgorithm_less import track_link_usage_total
 
 """
-Script to print link names and IDs in the format: ID 'from country' - 'to country'
-eg: 0 AT-CH
+Script to print link IDs  and labels.
 """
 
 def link_label(num,N):
@@ -23,9 +22,45 @@ def link_label(num,N):
     label = get_link_direction(num,N)
     return str(label[0].label)+'-'+str(label[1].label)
 
+def link_namer(N=None,F=None):
+    """
+    Return a list of link labels: 'from-to'
+    """
+    if N == None:
+        N = EU_Nodes_usage('linear.npz')
+    if F == None:
+        F = np.load('./results/linear-flows.npy')
+    lnames = []
+    for link in range(len(F)):
+        lnames.append(link_label(link,N))
+    return lnames
 
-N = EU_Nodes_usage('linear.npz')
-F = np.load('./results/linear-flows.npy')
+def node_namer(N=None):
+    """
+    Return a list of node labels eg. 'DK'
+    """
+    if N == None:
+        N = EU_Nodes_usage('linear.npz')
+    nnames = []
+    for node in N:
+        nnames.append(node.label)
+    return nnames
 
-for link in range(len(F)):
-    print link,link_label(link,N)
+def link_dict(N=None,F=None):
+    """
+    Return a dictionary of node labels and IDs of their directly connected links
+    eg. 'AT':[0,1,2,3,4,5]
+    """
+    if N == None:
+        N = EU_Nodes_usage('linear.npz')
+    if F == None:
+        F = np.load('./results/linear-flows.npy')
+    ldict = {}
+    for link in range(len(F)):
+        label = get_link_direction(link,N)
+        for l in label:
+            if str(l.label) in ldict:
+                ldict[str(l.label)].append(link)
+            else:
+                ldict.update({str(l.label):[link]})
+    return ldict
