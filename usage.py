38934c6e39e068952a8443b86219e7bf502ff4c3
Bo Tranberg <bo@tberg.dk>
2014-08-26 12:33:58 +0200

    new definition of bin edges

--- usage.py
+++ usage.py
@@ -45,7 +45,7 @@ def bin_maker(F_matrix,q,lapse):
     bin_max = np.ceil(q) # last bin ends at 99% quantile
     nbins = 12 # this number is not at all arbitrary
     bin_size = bin_max/nbins
-    bin_means = np.linspace(.5*bin_size,bin_max-(.5*bin_size),nbins) # mean values of bins
+    bin_edges = np.linspace(bin_size,bin_max),nbins) # value at the right side of each bin, last bin is 99% quantile
 
     H_temp = []
     H = np.zeros((nbins,2)) # [number of events, mean usage]
@@ -73,7 +73,7 @@ def bin_maker(F_matrix,q,lapse):
         H[b,0] = 0
         H[b,1] = 0
     H_temp=[]
-    return H,bin_means
+    return H,bin_edges
 
 def bin_prob(bin_id,H):
     """
@@ -81,7 +81,7 @@ def bin_prob(bin_id,H):
     """
     return H[bin_id,0]/sum(H[:,0])
 
-def bin_CDF(bin_id,H,bin_means):
+def bin_CDF(bin_id,H):
     """
     Cumulative distribution function to be used on the bins created in the
     bin_maker.
@@ -94,15 +94,15 @@ def bin_CDF(bin_id,H,bin_means):
     P = P/sum(H[:,0])
     return P
 
-def node_stake(H,bin_means):
+def node_stake(H,bin_edges):
     """
     Calculate a node's stake in a specific link
     """
-    flows = np.append([0],bin_means)
-    nbins = len(bin_means)
+    flows = np.append([0],bin_edges)
+    nbins = len(bin_edges)
     c = 0 # node stake
     for i in range(nbins-1):
-        c += (flows[i+1]-flows[i])/(1-bin_CDF(i,H,bin_means))
+        c += (flows[i+1]-flows[i])/(1-bin_CDF(i,H))
         for j in range(i+1,nbins):
             c += bin_prob(j,H)*H[j,1]
     return c
@@ -136,8 +136,8 @@ if 'solve' in task:
             F_matrix = np.hstack([F_vert,exp_vert]) # [flow, usage]
             F_matrix[F_matrix[:,0].argsort()]
             
-            H,bin_means = bin_maker(F_matrix,quantiles[link],lapse)
-            Node_stakes[node,link] = node_stake(H,bin_means)
+            H,bin_edges = bin_maker(F_matrix,quantiles[link],lapse)
+            Node_stakes[node,link] = node_stake(H,bin_edges)
             
     # save results to file for faster and better plotting in usage_plotting.py
     np.save('Node_stakes_linear_export.npy',Node_stakes)
