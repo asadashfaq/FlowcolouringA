92384d18687b7d3cd2f59707d10d73bc76a51701
Bo Tranberg <bo@tberg.dk>
2014-10-02 12:32:32 +0200

    Added regions and super reagions

--- EUgrid.py
+++ EUgrid.py
@@ -10,7 +10,12 @@ for i in range(30):
 	for j in range(11):
 		homo_alphas=np.append(homo_alphas,j/float(10))
 
+# Defining names of data files
 files = ['AT.npz', 'FI.npz', 'NL.npz', 'BA.npz', 'FR.npz', 'NO.npz', 'BE.npz', 'GB.npz', 'PL.npz', 'BG.npz', 'GR.npz', 'PT.npz', 'CH.npz', 'HR.npz', 'RO.npz', 'CZ.npz', 'HU.npz', 'RS.npz', 'DE.npz', 'IE.npz', 'SE.npz', 'DK.npz', 'IT.npz', 'SI.npz', 'ES.npz', 'LU.npz', 'SK.npz', 'EE.npz', 'LV.npz', 'LT.npz']
+
+regions = ['AT.npz', 'BA.npz', 'BA_off.npz', 'BE.npz', 'BE_off.npz', 'BG.npz', 'BG_off.npz', 'CH.npz', 'CZ.npz', 'DE_M.npz', 'DE_NE.npz', 'DE_NE_off.npz', 'DE_NW.npz', 'DE_NW_off.npz', 'DE_SE.npz', 'DE_SW.npz', 'DE_W.npz', 'DK_E.npz', 'DK_E_off.npz', 'DK_W.npz', 'DK_W_off.npz', 'ES_NW.npz', 'ES_NW_off.npz', 'ES_SE.npz', 'ES_SE_off.npz', 'ES_SW.npz', 'ES_SW_off.npz', 'FI_N.npz', 'FI_N_off.npz', 'FI_S.npz', 'FI_S_off.npz', 'FR_NE.npz', 'FR_NW.npz', 'FR_NW_off.npz', 'FR_SE.npz', 'FR_SE_off.npz', 'FR_SW.npz', 'FR_SW_off.npz', 'GB_E_off.npz', 'GB_N.npz', 'GB_N_off.npz', 'GB_S.npz', 'GB_W_off.npz', 'GR.npz', 'GR_off.npz', 'HR.npz', 'HR_off.npz', 'HU.npz', 'IE.npz', 'IE_N.npz', 'IE_off.npz', 'IT_N.npz', 'IT_N_off.npz', 'IT_S.npz', 'IT_S_off.npz', 'IT_Sar.npz', 'IT_Sic.npz', 'LU.npz', 'NL.npz', 'NL_off.npz', 'NO_M.npz', 'NO_N.npz', 'NO_S.npz', 'NO_S_off.npz', 'PL.npz', 'PL_off.npz', 'PT_M.npz', 'PT_M_off.npz', 'PT_N.npz', 'PT_N_off.npz', 'PT_S.npz', 'PT_S_off.npz', 'RO.npz', 'RO_off.npz', 'RS.npz', 'SE_M.npz', 'SE_M_off.npz', 'SE_N.npz', 'SE_N_off.npz', 'SE_S.npz', 'SE_S_off.npz', 'SI.npz', 'SK.npz']
+
+superRegions = ['DE.npz', 'EE.npz', 'FR.npz', 'HI.npz', 'IT.npz', 'SC.npz', 'SG.npz', 'UK.npz']
 
 def EU_Nodes(load_filename=None, full_load=False):
     return au.Nodes(admat='./settings/eadmat.txt', path='./data/', prefix = "ISET_country_", files=files, load_filename=load_filename, full_load=full_load, alphas=alphas, gammas=np.ones(30))
@@ -27,9 +32,18 @@ def EU_Nodes_log(load_filename=None, full_load=False,year=0): # used to create a
 def EU_Nodes_Ben(load_filename=None, full_load=False,alphas=None,gammas=None):
     return au.Nodes(admat='./settings/eadmat.txt', path='./data/', prefix = "ISET_country_", files=files, load_filename=load_filename, full_load=full_load, alphas=alphas, gammas=np.ones(30))
 
+# Nodes object for Bo's usage calculation, alphas=.7, gammas=1
 def EU_Nodes_usage(load_filename=None, full_load=False):
     return au.Nodes(admat='./settings/eadmat.txt', path='./data/', prefix = "ISET_country_", files=files, load_filename=load_filename, full_load=full_load, alphas=np.ones(30)*.7, gammas=np.ones(30))
 
+# Nodes object containing 83 regions, alphas=.7, gammas=1
+def EU_Nodes_regions(load_filename=None, full_load=False):
+    return au.Nodes(admat='./settings/regionadmat.txt', path='./data/regions/', prefix = "ISET_region_", files=regions, load_filename=load_filename, full_load=full_load, alphas=np.ones(len(regions))*.7, gammas=np.ones(len(regions)))
+
+# Nodes object containing 8 super regions, alphas=.7, gammas=1
+def EU_Nodes_superRegions(load_filename=None, full_load=False):
+    return au.Nodes(admat='./settings/superregionadmat.txt', path='./data/superregions/', prefix = "", files=superRegions, load_filename=load_filename, full_load=full_load, alphas=np.ones(len(superRegions))*.7, gammas=np.ones(len(superRegions)))
+	
 def linfo():
     link_info = open("line_info")
     LI = []
