2de192a2a71cccaa4effb3f8de40ca1ae60b50a5
Bo Tranberg <bo@tberg.dk>
2014-10-07 14:59:20 +0200

    Added calculation of contribution

--- sensitivity.py
+++ sensitivity.py
@@ -10,6 +10,7 @@ from EUgrid import *
 from link_colour_less import track_flows, get_link_direction
 from new_linkcolouralgorithm_less import track_link_usage_total
 from link_namer import node_namer,link_dict
+from usage import bin_maker, bin_prob, bin_CDF, node_contrib
 
 """
 Script to calculate nodes' usage of links in the same way as
@@ -38,8 +39,9 @@ if len(sys.argv)<2:
 else:
     task = str(sys.argv[1:])
 
-networks = ['regions','superRegions']
+networks = ['superRegions', 'regions']
 schemes = ['linear', 'square', 'RND']
+directions = ['import', 'export', 'combined']
 lapse = 70128  # number of hours to include
 
 def regionSolver(scheme):
@@ -87,6 +89,62 @@ def calc_usage(network):
         boxplot,boxplotlabel = track_link_usage_total(N2,F,mode=scheme,alph='same',lapse=lapse,sensitivity=True)
     return
 
+def calc_contribution(network,scheme,verbose=None):
+    """
+    Calculate nodes' contribution for a single mode and save results to file.
+    Adapted from _solver_ in usage.py
+    Set verbose to True to follow the progress in the command line. Not recommended for multi processing.
+    """
+    # pick one of three transmission paradigms
+    if verbose:
+        print('Scheme: '+scheme)
+    
+    if network == 'superRegions':
+        N = EU_Nodes_superRegions('../sensitivity/superRegions-'+scheme+'.npz')
+    elif network == 'regions':
+        N = EU_Nodes_regions('../sensitivity/regions-'+scheme+'.npz')
+    else:
+        raise Exception('Wrong network!')
+    
+    # Get 99% quantile of link flow
+    F = abs(np.load('./sensitivity/'+network+'-'+scheme+'-flows.npy'))
+    quantiles = [get_q(abs(F[link,:lapse]),.99) for link in range(len(F))]
+    
+    # Do everything below for both import and export usages unless we want the combined case
+    for direction in directions:
+        if verbose:
+            print('Direction: '+direction)
+        if 'combined' in directions:
+            Usages1 = np.load('./sensitivity/linkcolouring/'+network+'-old_'+scheme+'_copper_link_mix_import_all_alpha=same.npy')
+            Usages2 = np.load('./sensitivity/linkcolouring/'+network+'-old_'+scheme+'_copper_link_mix_export_all_alpha=same.npy')
+            Usages = Usages1+Usages2
+            Usages1,Usages2 = None,None
+        else:
+            Usages = np.load('./sensitivity/linkcolouring/'+network+'-old_'+scheme+'_copper_link_mix_'+direction+'_all_alpha=same.npy')
+        print('Loaded '+scheme+' usages')
+        
+        # Calculate usages and save to file
+        Node_contributions = np.zeros((len(N),len(F))) # empty array for calculated usages
+        for node in range(len(N)):
+            if verbose:
+                print node+1,'/ '+str(len(N))
+            for link in range(len(F)):
+                # Stacking and sorting data
+                F_vert = np.reshape(F[link,:lapse],(len(F[link,:lapse]),1))
+                exp_vert = np.reshape(Usages[link,node,:lapse],(len(Usages[link,node,:lapse]),1))
+                F_matrix = np.hstack([F_vert,exp_vert]) # [flow, usage]
+                F_matrix[F_matrix[:,0].argsort()]
+                
+                H,bin_edges = bin_maker(F_matrix,quantiles[link],lapse,N_bins)
+                Node_contributions[node,link] = node_contrib(H,bin_edges)
+                
+        # save results to file 
+        np.save('./sensitivity/'+network+'-Node_contrib_'+scheme+'_'+direction+'_'+str(lapse)+'.npy',Node_contributions)
+        print('Saved Node_contributions to ./sensitivity/'+network+'-Node_contrib_'+scheme+'_'+direction+'_'+str(lapse)+'.npy')
+        if ((direction == 'import') or ('combined' in directions)):
+            np.save('./sensitivity/'+network+'-quantiles_'+scheme+'_'+str(lapse)+'.npy',quantiles)
+            print('Saved 99% quantiles to ./sensitivity/'+network+'-quantiles_'+scheme+'_'+str(lapse)+'.npy')
+
 """
 Solving flows for different export schemes
 """
@@ -106,3 +164,13 @@ if 'trace' in task:
     print 'Lapse: '+str(lapse)+' hours.'
     for network in networks:
         calc_usage(network)
+
+"""
+Calculate nodes' contributions and save results to file.
+"""
+if 'usage' in task:
+    print 'Mode selected: Calculate usage'
+    N_bins = 16 # This number has been determined from a calculation of convergence. See 'convergence' in _usage.py_.
+    for network in networks:
+        for scheme in schemes:
+            calc_contribution(network,scheme)
