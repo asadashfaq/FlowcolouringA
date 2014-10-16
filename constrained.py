0d1d03b81d30960346ca3496acff44d55cf00875
Bo Tranberg <bo@tberg.dk>
2014-10-16 17:56:04 +0200

    added usage calculation

--- constrained.py
+++ constrained.py
@@ -6,11 +6,10 @@ import matplotlib.pyplot as plt
 from multiprocessing import Pool
 import aurespf.solvers as au
 from aurespf.tools import get_q, get_quant_caps
-#from EUgrid import *
 from link_colour_less import track_flows, get_link_direction
 from new_linkcolouralgorithm_less import track_link_usage_total
 from link_namer import node_namer,link_dict
-#from usage import bin_maker, bin_prob, bin_CDF, node_contrib
+from functions import binMaker, bin_prob, bin_CDF, node_contrib
 from europe_plusgrid import *
 
 """
@@ -21,6 +20,7 @@ All ouput files are saved to ./constrained/
 
 Call the script using only one of the following command line arguments:
 - trace:    run flow tracing and save results
+- usage:    calculate node's usage of links
 
 """
 
@@ -29,7 +29,7 @@ if len(sys.argv)<2:
 else:
     task = str(sys.argv[1:])
 
-def calc_usage(N,F,lapse,direction,b):
+def calc_usage(N,F,lapse,scheme,b):
     """
     Calculate powermixes and nodes' usages of links and save results to file.
     """
@@ -44,21 +44,83 @@ def calc_usage(N,F,lapse,direction,b):
     track_link_usage_total tracks each nodes usage of all links. The results
     are saved to files '..._links_ex_...' and '..._links_im_...'.
     """
-    boxplot,boxplotlabel = track_link_usage_total(N2,F,mode=direction,lapse=lapse,constrained=b)
+    boxplot,boxplotlabel = track_link_usage_total(N2,F,mode=scheme,lapse=lapse,constrained=b)
     return
 
+def calc_contribution(b, verbose=False):
+    """
+    Calculate nodes' contribution for a single scheme and save results to file.
+    Set verbose to True to follow the progress in the command line. Not recommended for multi processing.
+    """
+    for scheme in schemes:
+        if verbose:
+            print('scheme: '+scheme) 
+        F = abs(np.load('./ConstrainedFlowData/Europe_aHE_'+str(b)+'q99_DC_'+scheme+'_flows.npy'))
+        # Get 99% quantile of link flow
+        quantiles = [get_q(abs(F[link,:lapse]),.99) for link in range(len(F))]
+        
+        # Do everything below for both import and export usages unless we want the combined case
+        for direction in directions:
+            if verbose:
+                print('Direction: '+direction)
+            if direction == 'combined':
+                Usages1 = np.load('./constrained/linkcolouring/one_year/'+scheme+'_link_mix_import_b_'+str(b)+'.npy')
+                Usages2 = np.load('./constrained/linkcolouring/one_year/'+scheme+'_link_mix_export_b_'+str(b)+'.npy')
+                Usages = Usages1+Usages2
+                Usages1,Usages2 = None,None
+            else:
+                Usages = np.load('./constrained/linkcolouring/one_year/'+scheme+'_link_mix_'+str(direction)+'_b_'+str(b)+'.npy')
+            print('Loaded '+scheme+' usages')
+            
+            # Calculate usages and save to file
+            Node_contributions = np.zeros((nNodes,len(F))) # empty array for calculated usages
+            for node in range(nNodes):
+                if verbose:
+                    print node+1,'/ '+str(nNodes)
+                for link in range(len(F)):
+                    # Stacking and sorting data
+                    F_vert = np.reshape(F[link,:lapse],(len(F[link,:lapse]),1))
+                    exp_vert = np.reshape(Usages[link,node,:lapse],(len(Usages[link,node,:lapse]),1))
+                    F_matrix = np.hstack([F_vert,exp_vert]) # [flow, usage]
+                    F_matrix[F_matrix[:,0].argsort()]
+                    
+                    H,bin_edges = binMaker(F_matrix,quantiles[link],lapse,N_bins)
+                    Node_contributions[node,link] = node_contrib(H,bin_edges)
+                    
+            # save results to file 
+            np.save('./constrained/Node_contrib_'+scheme+'_'+direction+'_b_'+str(b)+'.npy',Node_contributions)
+            print('Saved Node_contributions to ./constrained/Node_contrib_'+scheme+'_'+direction+'_b_'+str(b)+'.npy')
+            if direction == 'import':
+                np.save('./constrained/quantiles_'+scheme+'_b_'+str(b)+'.npy',quantiles)
+                print('Saved 99% quantiles to ./constrained/quantiles_'+scheme+'_b_'+str(b)+'.npy')
+
 def caller(direction):
+    N = europe_plus_Nodes(load_filename='../ConstrainedFlowData/Europe_aHE_copper_DC_lin.npz')
 #    for b in np.linspace(0.05,1.5,30):
     for b in np.linspace(0.05,1.45,15):
         F = np.load('./ConstrainedFlowData/Europe_aHE_'+str(b)+'q99_DC_'+direction+'_flows.npy')
         calc_usage(N,F,lapse,direction,b)
 
+lapse = 8760 # 280512
+schemes = ['lin', 'sqr']
+directions = ['import', 'export', 'combined']
+
 """
 Calculate powermixes and nodes' usages of links and save results to file.
 """
 if 'trace' in task:
-	lapse = 87600 # 280512
-	N = europe_plus_Nodes(load_filename='../ConstrainedFlowData/Europe_aHE_copper_DC_lin.npz')
-	directions = ['lin', 'sqr']
-	p = Pool(2)
-	p.map(caller,directions)
+    print('Mode selected: flow tracing')
+    p = Pool(2)
+    p.map(caller,schemes)
+
+"""
+Calculate nodes' contributions and save results to file.
+"""
+if 'usage' in task:
+    print('Mode selected: usage calculation')
+    N = np.load('./ConstrainedFlowData/Europe_aHE_copper_DC_lin.npz', mmap_mode='r+')
+    nNodes = len(N['id'])
+    N_bins = 16
+    b = np.linspace(0.05,1.5,30)
+    p = Pool(8)
+    p.map(calc_contribution, b)
