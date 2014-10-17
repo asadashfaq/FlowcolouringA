7aec8f7f29d323a3df8e3b60d805fd88aef030a9
Bo Tranberg <bo@tberg.dk>
2014-10-17 17:24:39 +0200

    plotting of node network usage

--- constrained.py
+++ constrained.py
@@ -4,7 +4,6 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from multiprocessing import Pool
-import aurespf.solvers as au
 from aurespf.tools import get_q, get_quant_caps
 from link_colour_less import track_flows, get_link_direction
 from new_linkcolouralgorithm_less import track_link_usage_total
@@ -21,7 +20,7 @@ All ouput files are saved to ./constrained/
 Call the script using only one of the following command line arguments:
 - trace:    run flow tracing and save results
 - usage:    calculate node's usage of links
-
+- plot:		create figures of usage as function of transmission constraint
 """
 
 if len(sys.argv)<2:
@@ -94,12 +93,54 @@ def calc_contribution(b, verbose=False):
                 np.save('./constrained/quantiles_'+scheme+'_b_'+str(b)+'.npy',quantiles)
                 print('Saved 99% quantiles to ./constrained/quantiles_'+scheme+'_b_'+str(b)+'.npy')
 
-def caller(direction):
+def caller(scheme):
     N = europe_plus_Nodes(load_filename='../ConstrainedFlowData/Europe_aHE_copper_DC_lin.npz')
 #    for b in np.linspace(0.05,1.5,30):
     for b in np.linspace(0.05,1.45,15):
-        F = np.load('./ConstrainedFlowData/Europe_aHE_'+str(b)+'q99_DC_'+direction+'_flows.npy')
-        calc_usage(N,F,lapse,direction,b)
+        F = np.load('./ConstrainedFlowData/Europe_aHE_'+str(b)+'q99_DC_'+scheme+'_flows.npy')
+        calc_usage(N,F,lapse,scheme,b)
+
+def plotter(scheme):
+    """
+    Make a lot of figures for a single mode and put them in
+    ./constrained/figures/scheme/
+    """
+    # Country's usage of all links as function of b
+    for node in nodes:
+        plt.figure(figsize=(16,8))
+        usages = []
+        scheme='lin'
+        for b in np.linspace(0.05,1.5,30):
+            # Load data and results
+            F = abs(np.load('./ConstrainedFlowData/Europe_aHE_'+str(b)+'q99_DC_'+scheme+'_flows.npy'))
+            quantiles = np.load('./constrained/quantiles_'+scheme+'_b_'+str(b)+'.npy')
+            N_usages = np.load('./constrained/Node_contrib_'+scheme+'_combined_b_'+str(b)+'.npy')
+            usages.append(.5*N_usages[node]/quantiles)
+        usages = np.array(usages).transpose()
+        plt.subplot(121)
+        plt.pcolormesh(usages)
+        plt.colorbar()
+        plt.xlabel(r'$\beta$')
+        plt.ylabel('link number')
+        plt.title(names[node]+'\'s usage of links, linear')
+
+        usages = []
+        scheme='sqr'
+        for b in np.linspace(0.05,1.5,30):
+            # Load data and results
+            F = abs(np.load('./ConstrainedFlowData/Europe_aHE_'+str(b)+'q99_DC_'+scheme+'_flows.npy'))
+            quantiles = np.load('./constrained/quantiles_'+scheme+'_b_'+str(b)+'.npy')
+            N_usages = np.load('./constrained/Node_contrib_'+scheme+'_combined_b_'+str(b)+'.npy')
+            usages.append(.5*N_usages[node]/quantiles)
+        usages = np.array(usages).transpose()
+        plt.subplot(122)
+        plt.pcolormesh(usages)
+        plt.colorbar()
+        plt.xlabel(r'$\beta$')
+        plt.title(names[node]+'\'s usage of links, square')
+        plt.savefig('./constrained/figures/node-usage-'+str(node)+'.png', bbox_inches='tight')
+
+    # All countries usage of a single link as function of b
 
 lapse = 8760 # 280512
 schemes = ['lin', 'sqr']
@@ -124,3 +165,16 @@ if 'usage' in task:
     b = np.linspace(0.05,1.5,30)
     p = Pool(8)
     p.map(calc_contribution, b)
+
+"""
+Create various plots of usage and save figures to ./figures/
+"""
+if 'plot' in task:
+    N = europe_plus_Nodes(load_filename='../ConstrainedFlowData/Europe_aHE_copper_DC_lin.npz')
+    F = abs(np.load('./ConstrainedFlowData/Europe_aHE_0.05q99_DC_lin_flows.npy'))
+    link_dic = link_dict(N,F)
+    nodes = range(len(N))
+    links = range(len(F))
+    names = [str(N[i].label) for i in range(len(N))]
+    N = None
+    plotter('lin')
