0e0fd38e78acc7333bf157dce3e27053b62e351f
Bo Tranberg <bo@tberg.dk>
2014-10-19 15:11:06 +0200

    complete plotting of constrained usage

--- constrained.py
+++ constrained.py
@@ -7,9 +7,10 @@ from multiprocessing import Pool
 from aurespf.tools import get_q, get_quant_caps
 from link_colour_less import track_flows, get_link_direction
 from new_linkcolouralgorithm_less import track_link_usage_total
-from link_namer import node_namer,link_dict
+from link_namer import node_namer, link_namer, link_dict
 from functions import binMaker, bin_prob, bin_CDF, node_contrib
 from europe_plusgrid import *
+from EUgrid import EU_Nodes
 
 """
 Script to investigate the transition of a node's usage of the links as
@@ -100,15 +101,17 @@ def caller(scheme):
         F = np.load('./ConstrainedFlowData/Europe_aHE_'+str(b)+'q99_DC_'+scheme+'_flows.npy')
         calc_usage(N,F,lapse,scheme,b)
 
-def plotter(scheme):
+def plotter():
     """
     Make a lot of figures for a single mode and put them in
     ./constrained/figures/scheme/
+    This function is very ugly because it was written very fast without thinking much
     """
     # Country's usage of all links as function of b
     for node in nodes:
-        plt.figure(figsize=(16,8))
+        plt.figure(figsize=(16,7))
         usages = []
+        totalUsagesLin = []
         scheme='lin'
         for b in np.linspace(0.05,1.5,30):
             # Load data and results
@@ -116,13 +119,86 @@ def plotter(scheme):
             quantiles = np.load('./constrained/quantiles_'+scheme+'_b_'+str(b)+'.npy')
             N_usages = np.load('./constrained/Node_contrib_'+scheme+'_combined_b_'+str(b)+'.npy')
             usages.append(.5*N_usages[node]/quantiles)
+            totalUsagesLin.append(np.sum(.5*N_usages/quantiles,1))
         usages = np.array(usages).transpose()
-        plt.subplot(121)
+        ax1 = plt.subplot(121)
         plt.pcolormesh(usages)
         plt.colorbar()
         plt.xlabel(r'$\beta$')
         plt.ylabel('link number')
         plt.title(names[node]+'\'s usage of links, linear')
+        ax1.set_xticks(np.linspace(0,30,16))
+        ax1.set_xticklabels(np.linspace(0,1.5,16))
+        ax1.set_yticks(np.linspace(.5,49.5,50))
+        ax1.set_yticklabels(link_names,ha="right",va="center",fontsize=6)
+
+        usages = []
+        totalUsagesSqr = []
+        scheme='sqr'
+        for b in np.linspace(0.05,1.5,30):
+            # Load data and results
+            F = abs(np.load('./ConstrainedFlowData/Europe_aHE_'+str(b)+'q99_DC_'+scheme+'_flows.npy'))
+            quantiles = np.load('./constrained/quantiles_'+scheme+'_b_'+str(b)+'.npy')
+            N_usages = np.load('./constrained/Node_contrib_'+scheme+'_combined_b_'+str(b)+'.npy')
+            usages.append(.5*N_usages[node]/quantiles)
+            totalUsagesSqr.append(np.sum(.5*N_usages/quantiles,1))
+        usages = np.array(usages).transpose()
+        ax2 = plt.subplot(122)
+        plt.pcolormesh(usages)
+        plt.colorbar()
+        plt.xlabel(r'$\beta$')
+        plt.title(names[node]+'\'s usage of links, square')
+        ax2.set_xticks(np.linspace(0,30,16))
+        ax2.set_xticklabels(np.linspace(0,1.5,16))
+        ax2.set_yticks(np.linspace(.5,49.5,50))
+        ax2.set_yticklabels(link_names,ha="right",va="center",fontsize=6)
+        plt.savefig('./constrained/figures/node-usage-'+str(node)+'.png', bbox_inches='tight')
+
+    # Figure comparing nodes' total network usage as function of b
+    totalUsagesLin = np.array(totalUsagesLin).transpose()
+    totalUsagesSqr = np.array(totalUsagesSqr).transpose()
+    plt.figure(figsize=(16,7))
+    ax3 = plt.subplot(121)
+    ax3.set_xticks(np.linspace(0,30,16))
+    ax3.set_xticklabels(np.linspace(0,1.5,16))
+    ax3.set_yticks(np.linspace(.5,29.5,30))
+    ax3.set_yticklabels(names,ha="right",va="center",fontsize=9)
+    plt.pcolormesh(totalUsagesLin)
+    plt.colorbar()
+    plt.xlabel(r'$\beta$')
+    plt.title('Total network usage, linear')
+    ax4 = plt.subplot(122)
+    ax4.set_xticks(np.linspace(0,30,16))
+    ax4.set_xticklabels(np.linspace(0,1.5,16))
+    ax4.set_yticks(np.linspace(.5,29.5,30))
+    ax4.set_yticklabels(names,ha="right",va="center",fontsize=9)
+    plt.pcolormesh(totalUsagesSqr)
+    plt.colorbar()
+    plt.xlabel(r'$\beta$')
+    plt.title('Total network usage, square')
+    plt.savefig('./constrained/figures/total-network-usage.png', bbox_inches='tight')
+
+    # All countries usage of a single link as function of b
+    for link in links:
+        plt.figure(figsize=(16,7))
+        usages = []
+        scheme='lin'
+        for b in np.linspace(0.05,1.5,30):
+            # Load data and results
+            F = abs(np.load('./ConstrainedFlowData/Europe_aHE_'+str(b)+'q99_DC_'+scheme+'_flows.npy'))
+            quantiles = np.load('./constrained/quantiles_'+scheme+'_b_'+str(b)+'.npy')
+            N_usages = np.load('./constrained/Node_contrib_'+scheme+'_combined_b_'+str(b)+'.npy')
+            usages.append(.5*N_usages[:,link]/quantiles[link])
+        usages = np.array(usages).transpose()
+        ax5 = plt.subplot(121)
+        ax5.set_xticks(np.linspace(0,30,16))
+        ax5.set_xticklabels(np.linspace(0,1.5,16))
+        ax5.set_yticks(np.linspace(.5,29.5,30))
+        ax5.set_yticklabels(names,ha="right",va="center",fontsize=9)
+        plt.pcolormesh(usages)
+        plt.colorbar()
+        plt.xlabel(r'$\beta$')
+        plt.title('Usage of link '+link_names[link]+', linear')
 
         usages = []
         scheme='sqr'
@@ -131,16 +207,18 @@ def plotter(scheme):
             F = abs(np.load('./ConstrainedFlowData/Europe_aHE_'+str(b)+'q99_DC_'+scheme+'_flows.npy'))
             quantiles = np.load('./constrained/quantiles_'+scheme+'_b_'+str(b)+'.npy')
             N_usages = np.load('./constrained/Node_contrib_'+scheme+'_combined_b_'+str(b)+'.npy')
-            usages.append(.5*N_usages[node]/quantiles)
+            usages.append(.5*N_usages[:,link]/quantiles[link])
         usages = np.array(usages).transpose()
-        plt.subplot(122)
+        ax6 = plt.subplot(121)
+        ax6.set_xticks(np.linspace(0,30,16))
+        ax6.set_xticklabels(np.linspace(0,1.5,16))
+        ax6.set_yticks(np.linspace(.5,29.5,30))
+        ax6.set_yticklabels(names,ha="right",va="center",fontsize=9)
         plt.pcolormesh(usages)
         plt.colorbar()
         plt.xlabel(r'$\beta$')
-        plt.title(names[node]+'\'s usage of links, square')
-        plt.savefig('./constrained/figures/node-usage-'+str(node)+'.png', bbox_inches='tight')
-
-    # All countries usage of a single link as function of b
+        plt.title('Usage of link '+link_names[link]+', square')
+        plt.savefig('./constrained/figures/link-usage-'+str(link)+'.png', bbox_inches='tight')
 
 lapse = 8760 # 280512
 schemes = ['lin', 'sqr']
@@ -170,11 +248,12 @@ if 'usage' in task:
 Create various plots of usage and save figures to ./figures/
 """
 if 'plot' in task:
-    N = europe_plus_Nodes(load_filename='../ConstrainedFlowData/Europe_aHE_copper_DC_lin.npz')
+    N = EU_Nodes()
     F = abs(np.load('./ConstrainedFlowData/Europe_aHE_0.05q99_DC_lin_flows.npy'))
     link_dic = link_dict(N,F)
     nodes = range(len(N))
     links = range(len(F))
     names = [str(N[i].label) for i in range(len(N))]
+    link_names = link_namer(N,F)
     N = None
-    plotter('lin')
+    plotter()
