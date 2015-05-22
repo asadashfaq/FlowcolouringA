f1df851b9965ce7bd1e8c876dd428132a47c7c68
Bo Tranberg <bo@tberg.dk>
2015-05-22 15:40:53 +0200

    day/night figures

--- vector.py
+++ vector.py
@@ -17,6 +17,7 @@ Vector flow tracing on top of the up/down stream flow tracing algorithm.
 This script can be called from command line with one of the following inputs:
 - trace:                vectorize the flow tracing and save results
 - plot network:         load results from above and plot network figures for each color
+- plot network day:     same as above but split for daytime and nighttime
 - plot network total:   plot total network usage for each color
 - plot usage:           plot average usage for each color
 - sanity:               check whether the individual colors add to the scalar flow tracing
@@ -357,6 +358,133 @@ def drawnet_total(N=None, scheme='linear', direction='combined', color='solar'):
     # Save figure
     plt.savefig(figPath + "network/" + scheme + "/" + 'total_' + color + '_' + direction + ".png")
     plt.close()
+
+
+def drawnet_day(N=None, scheme='linear', direction='combined', color='solar'):
+    """
+    Make network figures for each color of the total network usage during the day and night.
+    """
+    colwidth = (3.425)
+    dcolwidth = (2 * 3.425 + 0.236)
+
+    if not N:
+        N = EU_Nodes_usage()
+    G = nx.Graph()
+    nodelist = []
+
+    # Add nodes and labels to networkx object for plotting
+    for n in N:
+        G.add_node(str(n.label))
+        nodelist.append(str(n.label))
+
+    # LF is a list of links
+    K, h, LF = AtoKh_old(N)
+
+    for l in LF:
+        G.add_edge(l[0], l[1], id=l[2])
+
+    # Define position of nodes
+    pos = {}
+    pos['AT'] = [0.55, 0.45]
+    pos['FI'] = [.95, 1.1]
+    pos['NL'] = [0.40, 0.85]
+    pos['BA'] = [0.65, 0.15]
+    pos['FR'] = [0.15, 0.60]
+    pos['NO'] = [0.5, 1.1]
+    pos['BE'] = [0.275, 0.775]
+    pos['GB'] = [0.10, 1.05]
+    pos['PL'] = [0.75, 0.8]
+    pos['BG'] = [0.9, 0.0]
+    pos['GR'] = [0.7, 0.0]
+    pos['PT'] = [0.0, 0.15]
+    pos['CH'] = [0.4, 0.45]
+    pos['HR'] = [0.75, 0.3]
+    pos['RO'] = [1.0, 0.15]
+    pos['CZ'] = [0.75, 0.60]
+    pos['HU'] = [1.0, 0.45]
+    pos['RS'] = [0.85, 0.15]
+    pos['DE'] = [0.45, 0.7]
+    pos['IE'] = [0.0, 0.95]
+    pos['SE'] = [0.75, 1.0]
+    pos['DK'] = [0.5, 0.875]
+    pos['IT'] = [0.4, 0.2]
+    pos['SI'] = [0.55, 0.3]
+    pos['ES'] = [0.15, 0.35]
+    pos['LU'] = [0.325, 0.575]
+    pos['SK'] = [0.90, 0.55]
+    pos['EE'] = [1.0, 0.985]
+    pos['LV'] = [0.975, 0.87]
+    pos['LT'] = [0.925, 0.77]
+
+    # Define color scale for links
+    blueDict = {'red': ((0.0, 1.0, 1.0), (0.3, 0.0, 0.0), (1.0, 0.0, 0.0)),
+                'green': ((0.0, 1.0, 1.0), (0.3, 0.0, 0.0), (1.0, 0.0, 0.0)),
+                'blue': ((0.0, 1.0, 1.0), (0.3, 1.0, 1.0), (1.0, 0.3, 0.3))}
+
+    orangeDict = {'red': ((0.0, 1.0, 1.0), (0.3, 1.0, 1.0), (1.0, 0.3, 0.3)),
+                  'green': ((0.0, 1.0, 1.0), (0.3, 0.65, .65), (1.0, .1, .1)),
+                  'blue': ((0.0, 1.0, 1.0), (0.3, 0.0, 0.0), (1.0, 0.0, 0.0))}
+
+    brownDict = {'red': ((0.0, 1.0, 1.0), (0.3, 0.57, 0.57), (1.0, 0.2, 0.2)),
+                 'green': ((0.0, 1.0, 1.0), (0.3, 0.36, .36), (1.0, .12, .12)),
+                 'blue': ((0.0, 1.0, 1.0), (0.3, 0.15, 0.15), (1.0, 0.01, 0.01))}
+
+    if color == 'wind':
+        cmap = LinearSegmentedColormap('blue', blueDict, 1000)
+    elif color == 'solar':
+        cmap = LinearSegmentedColormap('orange', orangeDict, 1000)
+    else:
+        cmap = LinearSegmentedColormap('brown', brownDict, 1000)
+
+    for time in ['day', 'night']:
+        # Load usages for given scheme and direction
+        N_usages = np.load(outPath + '/Node_contrib_' + scheme + '_' + direction + '_' + time + '_' + color + '.npy')
+        quantiles = np.load('./results/quantiles_' + str(scheme) + '_70128.npy')
+
+        # Calculate colors of links
+        linkUsages = np.sum(N_usages, 0) / quantiles
+        col = [(cmap(l)) for l in linkUsages]
+
+        # Create a new figure and plot network below
+        fig = plt.figure(dpi=400, figsize=(0.85 * dcolwidth, 0.85 * 0.8 * dcolwidth))
+
+        # color bar in bottom of figure
+        ax1 = fig.add_axes([0.05, 0.08, 0.9, .08])
+        cbl = mpl.colorbar.ColorbarBase(ax1, cmap, orientation='horizontal')
+
+        # Label just above color bar
+        if scheme == 'linear':
+            xlabel = 'Most localised'
+        elif scheme == 'square':
+            xlabel = 'Synchronised'
+        else:
+            xlabel = 'Market'
+        # ax1.set_xlabel(xlabel+' '+direction+r" usage $C_n/C^{\,99\%}$")
+        ax1.set_xlabel(r'$\mathcal{K}_{l}/\mathcal{K}^T_l$')
+        ax1.xaxis.set_label_position('top')
+
+        ax2 = fig.add_axes([-0.05, 0.15, 1.1, 0.95])
+
+        # Set color of nodes and draw all
+        node_c = ["#000000" for node in N]
+        nx.draw_networkx_nodes(G, pos, node_size=500, nodelist=nodelist, node_color=node_c, facecolor=(1, 1, 1))
+
+        # Draw links colored by usage of node n
+        edges = [(u, v) for (u, v, d) in G.edges(data=True)]
+        edge_id = [d['id'] for (u, v, d) in G.edges(data=True)]
+
+        color_sort = []
+        for i in range(len(col)):
+            color_sort.append(col[edge_id[i]])
+        nx.draw_networkx_edges(G, pos, edgelist=edges, width=3.5, edge_color=color_sort, alpha=1)
+
+        # Draw country names
+        nx.draw_networkx_labels(G, pos, font_size=12, font_color='w', font_family='sans-serif')
+        ax2.axis('off')
+
+        # Save figure
+        plt.savefig(figPath + "day/" + scheme + "/" + color + '_' + direction + '_' + time + ".png")
+        plt.close()
 
 
 def usagePlotter(direction):
@@ -708,10 +836,6 @@ if 'trace' in task:
 
 if 'plot' in task:
     if 'network' in task:
-        if 'total' in task:
-            print('Plotting total network figures')
-        else:
-            print('Plotting network figures')
         N = EU_Nodes_usage()
         colors = ['solar', 'wind']
         for mode in modes:
@@ -722,8 +846,13 @@ if 'plot' in task:
                 print('Direction: ' + direction)
                 for color in colors:
                     if 'total' in task:
+                        print('Plotting total network figures')
                         drawnet_total(N, mode, direction, color)
+                    elif 'day' in task:
+                        print('Plotting day/night network figures')
+                        drawnet_day(N, mode, direction, color)
                     else:
+                        print('Plotting network figures')
                         drawnet_usage(N, mode, direction, color)
 
     if 'usage' in task:
